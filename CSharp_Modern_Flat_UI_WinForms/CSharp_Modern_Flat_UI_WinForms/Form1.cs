78c8e0cad7725eb25c5360479408710c7e23efd3
hafizecim <hfz.snyl@gmail.com>
2025-11-01 20:13:18 +0300

    customizeDesing

--- CSharp_Modern_Flat_UI_WinForms/CSharp_Modern_Flat_UI_WinForms/Form1.cs
+++ CSharp_Modern_Flat_UI_WinForms/CSharp_Modern_Flat_UI_WinForms/Form1.cs
@@ -5,12 +5,21 @@ namespace CSharp_Modern_Flat_UI_WinForms
         public Form1()
         {
             InitializeComponent();
+            customizeDesing();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+        private void customizeDesing()
+        {
+            panelMediaSubmenu.Visible = false;
+            panelPlaylistSubMenu.Visible = false;
+            panel2.Visible = false;
+
+        }
+
 
     }
 }
